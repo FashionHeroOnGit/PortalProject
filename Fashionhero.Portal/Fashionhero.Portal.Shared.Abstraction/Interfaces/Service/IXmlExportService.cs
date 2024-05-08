4f32fa7d0bc60428069ba0270e48b2b8c8950a8c
andr9528 <asm@fashionhero.dk>
2024-05-08 10:43:42 +0200

    chore: creates 2 service interfaces

---
+++ Fashionhero.Portal/Fashionhero.Portal.Shared.Abstraction/Interfaces/Service/IXmlExportService.cs
@@ -0,0 +1,11 @@
+﻿namespace Fashionhero.Portal.Shared.Abstraction.Interfaces.Service
+{
+    public interface IXmlExportService
+    {
+        /// <summary>
+        /// Generates a Xml file at the specified output location, with structure corresponding to the instance supplied.
+        /// </summary>
+        /// <param name="outputPath"></param>
+        void GenerateXmlFile(string outputPath);
+    }
+}
