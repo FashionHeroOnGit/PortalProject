68d20c3cee1720aae258078698e63a23d628f02b
andr9528 <asm@fashionhero.dk>
2024-05-08 14:25:24 +0200

    chore: creates base classes for Entity Framework Core setup

---
+++ Fashionhero.Portal/Fashionhero.Portal.DataAccess/Core/BaseEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Fashionhero.Portal.Shared.Abstraction.Interfaces.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fashionhero.Portal.DataAccess.Core
+{
+    public abstract class BaseEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+        where TEntity : class, IEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            Type type = typeof(TEntity);
+            var idName = $"{type.Name}Id";
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).HasColumnName(idName);
+        }
+
+        protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<object>(x => { });
+        }
+    }
+}
