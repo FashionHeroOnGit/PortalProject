899ef8d01cecf4c1cc3b43598a7d4dc588cf34c1
andr9528 <asm@fashionhero.dk>
2024-05-13 15:30:18 +0200

    chore: completes mapping of xml files to model in memory

--- Fashionhero.Portal/Fashionhero.Portal.BusinessLogic/Extensions/XmlExtensions.cs
+++ Fashionhero.Portal/Fashionhero.Portal.BusinessLogic/Extensions/XmlExtensions.cs
@@ -1,28 +1,142 @@
 ﻿using System.Xml.Linq;
+using Microsoft.Extensions.Logging;
 
 namespace Fashionhero.Portal.BusinessLogic.Extensions
 {
     public static class XmlExtensions
     {
-        public static int GetValueAsInt(this XElement? element)
+        public static bool IsEmptyValue(this XElement? element)
         {
             if (element == null)
                 throw new ArgumentNullException(nameof(element));
 
-            bool success = int.TryParse(element.Value, out int result);
-
-            if (!success)
-                throw new InvalidCastException("Failed to cast value of xml element to int");
-
-            return result;
+            return element.Value.Length == 0;
         }
 
-        public static string GetValue(this XElement? element)
+        public static string GetTagValueAsString(this XElement element, string tag, ILogger logger)
         {
-            if (element == null)
-                throw new ArgumentNullException(nameof(element));
+            try
+            {
+                XElement taggedElement = element.GetTaggedElement(tag);
 
-            return element.Value;
+                return taggedElement.Value;
+            }
+            catch (ArgumentException ae)
+            {
+                logger.LogError(ae, $"Unknown tag Supplied");
+                throw;
+            }
+            catch (Exception e)
+            {
+                logger.LogWarning(e,
+                    $"Failed to get tag ({tag}) value from element ({element}). Returning empty string.");
+                return string.Empty;
+            }
+        }
+
+        private static XElement GetTaggedElement(this XElement element, string tag)
+        {
+            XElement? inner = element.Element(tag);
+            if (inner == null)
+                throw new ArgumentException($"Missing Tag ({tag}) in supplied element ({element})");
+            return inner;
+        }
+
+        public static int GetTagValueAsInt(this XElement element, string tag, ILogger logger)
+        {
+            try
+            {
+                XElement taggedElement = element.GetTaggedElement(tag);
+
+                bool success = int.TryParse(taggedElement.Value, out int result);
+
+                if (!success)
+                    throw new InvalidCastException("Failed to cast value of xml element to int");
+
+                return result;
+            }
+            catch (ArgumentException ae)
+            {
+                logger.LogError(ae, $"Unknown tag Supplied");
+                throw;
+            }
+            catch (InvalidCastException ice)
+            {
+                logger.LogWarning(ice,
+                    $"Unable to cast tag ({tag}) of element ({element}) to int. Returning default value.");
+                return default;
+            }
+            catch (Exception e)
+            {
+                logger.LogWarning(e,
+                    $"Failed to get tag ({tag}) value from element ({element}). Returning default value.");
+                return default;
+            }
+        }
+
+        public static long GetTaggedValueAsLong(this XElement element, string tag, ILogger logger)
+        {
+            try
+            {
+                XElement taggedElement = element.GetTaggedElement(tag);
+
+                bool success = long.TryParse(taggedElement.Value, out long result);
+
+                if (!success)
+                    throw new InvalidCastException("Failed to cast value of xml element to int");
+
+                return result;
+            }
+            catch (ArgumentException ae)
+            {
+                logger.LogError(ae, $"Unknown tag Supplied");
+                throw;
+            }
+            catch (InvalidCastException ice)
+            {
+                logger.LogWarning(ice,
+                    $"Unable to cast tag ({tag}) of element ({element}) to float. Returning default value.");
+                return default;
+            }
+            catch (Exception e)
+            {
+                logger.LogWarning(e,
+                    $"Failed to get tag ({tag}) value from element ({element}). Returning default value.");
+                return default;
+            }
+        }
+
+        public static float GetTaggedValueAsFloat(this XElement element, string tag, ILogger logger)
+        {
+            try
+            {
+                XElement taggedElement = element.GetTaggedElement(tag);
+
+                string toParse = taggedElement.Value.Split(' ')[0];
+                bool success = float.TryParse(toParse.Length == 0 ? "0" : toParse, out float result);
+
+                if (!success)
+                    throw new InvalidCastException("Failed to cast value of xml element to int");
+
+                return result;
+            }
+            catch (ArgumentException ae)
+            {
+                logger.LogError(ae, $"Unknown tag Supplied");
+                throw;
+            }
+            catch (InvalidCastException ice)
+            {
+                logger.LogWarning(ice,
+                    $"Unable to cast tag ({tag}) of element ({element}) to float. Returning default value.");
+                return default;
+            }
+            catch (Exception e)
+            {
+                logger.LogWarning(e,
+                    $"Failed to get tag ({tag}) value from element ({element}). Returning default value.");
+                return default;
+            }
         }
     }
 }
--- Fashionhero.Portal/Fashionhero.Portal.BusinessLogic/LoaderService.cs
+++ Fashionhero.Portal/Fashionhero.Portal.BusinessLogic/LoaderService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Xml;
 using System.Xml.Linq;
 using Fashionhero.Portal.BusinessLogic.Extensions;
+using Fashionhero.Portal.Shared.Abstraction.Enums;
 using Fashionhero.Portal.Shared.Abstraction.Interfaces.Model.Entity;
 using Fashionhero.Portal.Shared.Abstraction.Interfaces.Service;
 using Fashionhero.Portal.Shared.Model.Entity;
@@ -10,7 +11,6 @@ namespace Fashionhero.Portal.BusinessLogic
 {
     public class LoaderService : IXmlLoaderService
     {
-        private readonly ILogger<LoaderService> logger;
         private const string LOCALE_ROOT = "products";
         private const string LOCALE_SINGLE_PRODUCT = "product";
         private const string LOCALE_ID = "id";
@@ -24,6 +24,33 @@ namespace Fashionhero.Portal.BusinessLogic
         private const string LOCALE_MATERIALE = "materiale";
         private const string LOCALE_COUNTRY_OF_ORIGIN = "country_of_origin";
 
+        private const string INVENTORY_ROOT = "products";
+        private const string INVENTORY_SINGLE_PRODUCT = "product";
+        private const string INVENTORY_ID = "id";
+        private const string INVENTORY_LINK = "link";
+        private const string INVENTORY_IMAGE_LINK = "image_link";
+
+        private const string
+            INVENTORY_AVAILABILITY = "availability"; // Unused as 'Availability' is calculated from stock
+
+        private const string INVENTORY_REGULAR_PRICE = "regular_price";
+        private const string INVENTORY_SALE_PRICE = "sale_price";
+        private const string INVENTORY_PRICE_EUR = "price_eur";
+        private const string INVENTORY_SALE_PRICE_EUR = "sale_price_eur";
+        private const string INVENTORY_PRICE_SEK = "price_sek";
+        private const string INVENTORY_SALE_PRICE_SEK = "sale_price_sek";
+        private const string INVENTORY_PRICE_PLN = "price_pln";
+        private const string INVENTORY_SALE_PRICE_PLN = "sale_price_pln";
+        private const string INVENTORY_MODEL_PRODUCT_NUMBER = "mpn";
+        private const string INVENTORY_BRAND = "brand";
+        private const string INVENTORY_EAN = "gtin";
+        private const string INVENTORY_STOCK = "stock";
+        private const string INVENTORY_SIZE_A = "size-a";
+        private const string INVENTORY_SIZE_B = "size-b";
+        private const string INVENTORY_SPARTOO_KODE = "spartoo-kode";
+        private const string INVENTORY_PRODUCT_CATEGORY = "product_category";
+        private readonly ILogger<LoaderService> logger;
+
 
         public LoaderService(ILogger<LoaderService> logger)
         {
@@ -34,6 +61,145 @@ namespace Fashionhero.Portal.BusinessLogic
         public async Task GenerateInventory(ICollection<string> languagePaths, string inventoryPath)
         {
             var localeProducts = await ProcessLanguagePaths(languagePaths);
+            var sizes = (await GetSizes(inventoryPath)).Where(x => x.Quantity > 0).ToList();
+            var products = await GetMasterProducts(inventoryPath, localeProducts, sizes);
+        }
+
+        private async Task<ICollection<IProduct>> GetMasterProducts(
+            string inventoryPath, ICollection<ILocaleProduct> localeProducts, ICollection<ISize> sizes)
+        {
+            if (!inventoryPath.EndsWith(".xml"))
+                throw new ArgumentException("The supplied inventory path is not an .xml file.");
+
+            XDocument document = await GetDocument(inventoryPath);
+
+            logger.LogWarning(
+                $"{nameof(Product)}s' '{nameof(Product.Manufacturer)}' attribute is set to the value of the '{INVENTORY_BRAND}' tag, " +
+                $"as the Spartoo description of the expected value sounds like it being the brand.");
+
+            var xmlProducts = document.Elements(INVENTORY_ROOT).Elements(INVENTORY_SINGLE_PRODUCT).Where(x =>
+            {
+                XElement? element = x.Element(INVENTORY_LINK);
+                return element != null && !element.Value.Contains('?');
+            });
+            var generateTasks = xmlProducts.Select(x => GenerateProduct(x, localeProducts, sizes));
+            return await Task.WhenAll(generateTasks);
+        }
+
+        private Task<Product> GenerateProduct(
+            XElement element, ICollection<ILocaleProduct> localeProducts, ICollection<ISize> sizes)
+        {
+            string[] splitLink = element.GetTagValueAsString(INVENTORY_LINK, logger).Split('?');
+            var newProduct = new Product
+            {
+                ReferenceId = element.GetTagValueAsInt(INVENTORY_ID, logger),
+                LinkBase = splitLink[0],
+                Brand = element.GetTagValueAsString(INVENTORY_BRAND, logger),
+                Category = element.GetTagValueAsString(INVENTORY_PRODUCT_CATEGORY, logger),
+                Manufacturer = element.GetTagValueAsString(INVENTORY_BRAND, logger),
+                Sizes = sizes.Where(x => x.LinkBase == splitLink[0]).ToList(),
+                ExtraTags = GetExtraTags(element),
+                Images = GetImages(element),
+                Prices = GetPrices(element),
+            };
+
+            newProduct.Locales = localeProducts.Where(x => x.ItemGroupId == newProduct.ReferenceId).ToList();
+
+            return Task.FromResult(newProduct);
+        }
+
+        private ICollection<IPrice> GetPrices(XElement element)
+        {
+            return new List<IPrice>
+            {
+                new Price
+                {
+                    NormalSell = element.GetTaggedValueAsFloat(INVENTORY_REGULAR_PRICE, logger),
+                    Discount = element.GetTaggedValueAsFloat(INVENTORY_SALE_PRICE, logger),
+                    Currency = CurrencyCode.DKK,
+                },
+                new Price
+                {
+                    NormalSell = element.GetTaggedValueAsFloat(INVENTORY_PRICE_EUR, logger),
+                    Discount = element.GetTaggedValueAsFloat(INVENTORY_SALE_PRICE_EUR, logger),
+                    Currency = CurrencyCode.EUR,
+                },
+                new Price
+                {
+                    NormalSell = element.GetTaggedValueAsFloat(INVENTORY_PRICE_SEK, logger),
+                    Discount = element.GetTaggedValueAsFloat(INVENTORY_SALE_PRICE_SEK, logger),
+                    Currency = CurrencyCode.SEK,
+                },
+                new Price
+                {
+                    NormalSell = element.GetTaggedValueAsFloat(INVENTORY_PRICE_PLN, logger),
+                    Discount = element.GetTaggedValueAsFloat(INVENTORY_SALE_PRICE_PLN, logger),
+                    Currency = CurrencyCode.PLN,
+                },
+            };
+        }
+
+        private ICollection<IImage> GetImages(XElement element)
+        {
+            return new List<IImage>
+            {
+                new Image
+                {
+                    Url = element.GetTagValueAsString(INVENTORY_IMAGE_LINK, logger),
+                },
+            };
+        }
+
+        private ICollection<ITag> GetExtraTags(XElement element)
+        {
+            return new List<ITag>
+            {
+                new Tag
+                {
+                    Name = INVENTORY_SPARTOO_KODE,
+                    Value = element.GetTagValueAsString(INVENTORY_SPARTOO_KODE, logger),
+                },
+            };
+        }
+
+        private async Task<ICollection<ISize>> GetSizes(string inventoryPath)
+        {
+            if (!inventoryPath.EndsWith(".xml"))
+                throw new ArgumentException("The supplied inventory path is not an .xml file.");
+
+            XDocument document = await GetDocument(inventoryPath);
+
+            var xmlSizes = document.Elements(INVENTORY_ROOT).Elements(INVENTORY_SINGLE_PRODUCT).Where(x =>
+            {
+                XElement? element = x.Element(INVENTORY_LINK);
+                return element != null && element.Value.Contains('?');
+            }).Where(x => !x.Element(INVENTORY_EAN).IsEmptyValue());
+
+            var generateTasks = xmlSizes.Select(GenerateSize);
+            return await Task.WhenAll(generateTasks);
+        }
+
+        private Task<Size> GenerateSize(XElement element)
+        {
+            string[] splitLink = element.GetTagValueAsString(INVENTORY_LINK, logger).Split('?');
+
+            return Task.FromResult(new Size
+            {
+                Quantity = element.GetTagValueAsInt(INVENTORY_STOCK, logger),
+                LinkBase = splitLink[0],
+                LinkPostFix = $"?{splitLink[1]}",
+                Ean = element.GetTaggedValueAsLong(INVENTORY_EAN, logger),
+                ReferenceId = element.GetTagValueAsInt(INVENTORY_ID, logger),
+                ModelProductNumber = element.GetTagValueAsString(INVENTORY_MODEL_PRODUCT_NUMBER, logger),
+                Primary = element.GetTagValueAsString(INVENTORY_SIZE_A, logger),
+                Secondary = element.GetTagValueAsString(INVENTORY_SIZE_B, logger),
+            });
+        }
+
+        private static Task<XDocument> GetDocument(string path)
+        {
+            return XDocument.LoadAsync(XmlReader.Create(File.OpenRead(path), new XmlReaderSettings {Async = true,}),
+                LoadOptions.PreserveWhitespace, CancellationToken.None);
         }
 
         private async Task<ICollection<ILocaleProduct>> ProcessLanguagePaths(ICollection<string> languagePaths)
@@ -49,34 +215,33 @@ namespace Fashionhero.Portal.BusinessLogic
 
         private async Task<ICollection<ILocaleProduct>> GenerateLocaleProducts(string languagePath)
         {
-            XDocument document = await XDocument.LoadAsync(
-                XmlReader.Create(File.OpenRead(languagePath), new XmlReaderSettings {Async = true,}),
-                LoadOptions.PreserveWhitespace, CancellationToken.None);
+            XDocument document = await GetDocument(languagePath);
 
-            var xmlProducts = document.Elements(LOCALE_ROOT).Elements(LOCALE_SINGLE_PRODUCT);
-
-            return xmlProducts.Select(x => GenerateLocaleProduct(x, languagePath)).ToList();
+            var xmlLocaleProducts = document.Elements(LOCALE_ROOT).Elements(LOCALE_SINGLE_PRODUCT);
+            var generateTasks = xmlLocaleProducts.Select(x => GenerateLocaleProduct(x, languagePath));
+            return await Task.WhenAll(generateTasks);
         }
 
-        private ILocaleProduct GenerateLocaleProduct(XElement element, string languagePath)
+        private Task<LocaleProduct> GenerateLocaleProduct(XElement element, string languagePath)
         {
-            return new LocaleProduct
+            return Task.FromResult(new LocaleProduct
             {
                 IsoName = GetIsoName(languagePath),
-                ReferenceId = element.Element(LOCALE_ID).GetValueAsInt(),
-                Title = element.Element(LOCALE_TITLE).GetValue(),
-                Description = element.Element(LOCALE_DESCRIPTION).GetValue(),
-                ItemGroupId = element.Element(LOCALE_ITEM_GROUP_ID).GetValueAsInt(),
-                Type = element.Element(LOCALE_PRODUCT_TYPE).GetValue(),
-                LocalType = element.Element(LOCALE_PRODUCT_TYPE_LOCAL).GetValue(),
-                Colour = element.Element(LOCALE_COLOR).GetValue(),
-                Gender = element.Element(LOCALE_GENDER).GetValue(),
-                Material = element.Element(LOCALE_MATERIALE).GetValue(),
-                CountryOrigin = element.Element(LOCALE_COUNTRY_OF_ORIGIN).GetValue(),
-            };
+                ReferenceId = element.GetTagValueAsInt(LOCALE_ID, logger),
+                Title = element.GetTagValueAsString(LOCALE_TITLE, logger),
+                Description = element.GetTagValueAsString(LOCALE_DESCRIPTION, logger),
+                ItemGroupId = element.GetTagValueAsInt(LOCALE_ITEM_GROUP_ID, logger),
+                Type = element.GetTagValueAsString(LOCALE_PRODUCT_TYPE, logger),
+                LocalType = element.GetTagValueAsString(LOCALE_PRODUCT_TYPE_LOCAL, logger),
+                Colour = element.GetTagValueAsString(LOCALE_COLOR, logger),
+                Gender = element.GetTagValueAsString(LOCALE_GENDER, logger),
+                Material = element.GetTagValueAsString(LOCALE_MATERIALE, logger),
+                CountryOrigin = element.GetTagValueAsString(LOCALE_COUNTRY_OF_ORIGIN, logger),
+            });
         }
 
-        private string GetIsoName(string languagePath)
+
+        private static string GetIsoName(string languagePath)
         {
             string[] split = new FileInfo(languagePath).Name.Split('_');
             return split.Length == 1 ? "dk" : split[0];
