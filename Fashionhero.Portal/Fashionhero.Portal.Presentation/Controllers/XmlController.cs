81629ae3228038944a6abaf8dee8315f748d9cb0
andr9528 <asm@fashionhero.dk>
2024-05-29 15:16:45 +0200

    chore: updates resulting filename to not be different between generations

--- Fashionhero.Portal/Fashionhero.Portal.Presentation/Controllers/XmlController.cs
+++ Fashionhero.Portal/Fashionhero.Portal.Presentation/Controllers/XmlController.cs
@@ -31,7 +31,7 @@ namespace Fashionhero.Portal.Presentation.Controllers
         public async Task<IActionResult> GetSpartooFile()
         {
             const string contentType = "text/xml";
-            var fileName = $"SpartooImport-{DateOnly.FromDateTime(DateTime.Now).ToShortDateString()}.xml";
+            const string fileName = $"ExportToSpartoo.xml";
 
             try
             {
