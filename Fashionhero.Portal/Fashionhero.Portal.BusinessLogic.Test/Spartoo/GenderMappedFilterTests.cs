5b209a2394dc66405190ab24cf9e78cea82940b0
andr9528 <asm@fashionhero.dk>
2024-05-30 12:05:32 +0200

    test: adds tests for GenderMappedFilter

---
+++ Fashionhero.Portal/Fashionhero.Portal.BusinessLogic.Test/Spartoo/GenderMappedFilterTests.cs
@@ -0,0 +1,230 @@
+﻿using Fashionhero.Portal.BusinessLogic.Spartoo;
+using Fashionhero.Portal.BusinessLogic.Test.Core;
+using Fashionhero.Portal.BusinessLogic.Test.Extensions;
+using Fashionhero.Portal.Shared.Abstraction.Enums;
+using Fashionhero.Portal.Shared.Abstraction.Interfaces.Model.Entity;
+using Fashionhero.Portal.Shared.Model.Entity;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Fashionhero.Portal.BusinessLogic.Test.Spartoo
+{
+    public class GenderMappedFilterTests
+    {
+        private readonly Mock<ILogger<GenderMappedFilter>> mockedLogger;
+
+        public GenderMappedFilterTests()
+        {
+            mockedLogger = new Mock<ILogger<GenderMappedFilter>>();
+        }
+
+        [Fact]
+        public void ItDoesNotRemovesProductsWhenApplyingTheFilter()
+        {
+            var expected = GenerateValidProducts();
+            var original = GenerateValidProducts();
+            var sut = new GenderMappedFilter(mockedLogger.Object);
+
+            var actual = sut.FilterProducts(original);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ItLogsWarningWhenFilterDiscardsProductBecauseOfInvalidGender()
+        {
+            var expectedLogMessageFragment = "is not convertible to a";
+            var original = GenerateInvalidProductsWithInvalidGenders();
+            var sut = new GenderMappedFilter(mockedLogger.Object);
+
+            sut.FilterProducts(original);
+
+            mockedLogger.VerifyLogWarningCalled();
+            IInvocation? logInvocation = mockedLogger.TryGetInvocation(expectedLogMessageFragment);
+            logInvocation.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ItLogsWarningWhenFilterDiscardsProductBecauseOfMissingDanishTranslation()
+        {
+            var expectedLogMessageFragment = "as no Danish Translation of products";
+            var original = GenerateInvalidProductsWithMissingDanishTranslation();
+            var sut = new GenderMappedFilter(mockedLogger.Object);
+
+            sut.FilterProducts(original);
+
+            mockedLogger.VerifyLogWarningCalled();
+            IInvocation? logInvocation = mockedLogger.TryGetInvocation(expectedLogMessageFragment);
+            logInvocation.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ItRemovesInvalidProductsWhenApplyingTheFilter()
+        {
+            var expected = GenerateEmptyProductsList();
+            var original = GenerateInvalidProductsWithMissingDanishTranslation();
+            var sut = new GenderMappedFilter(mockedLogger.Object);
+
+            var actual = sut.FilterProducts(original);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ItReturnsDefaultValueWhenKeyDoesNotExistInDictionary()
+        {
+            char expected = default;
+            var sut = new GenderMappedFilter(mockedLogger.Object);
+
+            object actual = sut.GetDictionaryValue("some random text that is not a key in the dictionary");
+
+            actual.Should().BeOfType<char>();
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void ItReturnsValueFromDictionaryWhenKeyExists()
+        {
+            var expected = 'H';
+            var sut = new GenderMappedFilter(mockedLogger.Object);
+
+            object actual = sut.GetDictionaryValue("Mand");
+
+            actual.Should().BeOfType<char>();
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void ItSaysFalseWhenAskedIfFilterIsAnythingOtherThanGenderFilter()
+        {
+            const bool expected = false;
+            var sut = new GenderMappedFilter(mockedLogger.Object);
+
+            bool actual = sut.IsFilterOfType(FilterType.SPARTOO_CURRENCY);
+
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void ItSaysFalseWhenAskedIfFilterIsAnythingOtherThanGenderMapper()
+        {
+            const bool expected = false;
+            var sut = new GenderMappedFilter(mockedLogger.Object);
+
+            bool actual = sut.IsMapperOfType(MapType.SPARTOO_COLOUR);
+
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void ItSaysTrueWhenAskedIfFilterIsGenderFilter()
+        {
+            const bool expected = true;
+            var sut = new GenderMappedFilter(mockedLogger.Object);
+
+            bool actual = sut.IsFilterOfType(FilterType.SPARTOO_GENDER);
+
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void ItSaysTrueWhenAskedIfFilterIsGenderMapper()
+        {
+            const bool expected = true;
+            var sut = new GenderMappedFilter(mockedLogger.Object);
+
+            bool actual = sut.IsMapperOfType(MapType.SPARTOO_GENDER);
+
+            actual.Should().Be(expected);
+        }
+
+        private ICollection<IProduct> GenerateEmptyProductsList()
+        {
+            return TestEntitiesBuilder.BuildProducts([]).Cast<IProduct>().ToList();
+        }
+
+        private ICollection<IProduct> GenerateInvalidProductsWithInvalidGenders()
+        {
+            return TestEntitiesBuilder.BuildProducts([
+                new Product
+                {
+                    Locales = new List<ILocaleProduct>
+                    {
+                        new LocaleProduct
+                        {
+                            IsoName = "dk",
+                            Gender = "invalid",
+                        },
+                    },
+                },
+                new Product
+                {
+                    Locales = new List<ILocaleProduct>
+                    {
+                        new LocaleProduct
+                        {
+                            IsoName = "dk",
+                            Gender = "invalid",
+                        },
+                    },
+                },
+            ]).Cast<IProduct>().ToList();
+        }
+
+        private ICollection<IProduct> GenerateInvalidProductsWithMissingDanishTranslation()
+        {
+            return TestEntitiesBuilder.BuildProducts([
+                new Product
+                {
+                    Locales = new List<ILocaleProduct>
+                    {
+                        new LocaleProduct
+                        {
+                            IsoName = "en",
+                        },
+                    },
+                },
+                new Product
+                {
+                    Locales = new List<ILocaleProduct>
+                    {
+                        new LocaleProduct
+                        {
+                            IsoName = "en",
+                        },
+                    },
+                },
+            ]).Cast<IProduct>().ToList();
+        }
+
+        private ICollection<IProduct> GenerateValidProducts()
+        {
+            return TestEntitiesBuilder.BuildProducts([
+                new Product
+                {
+                    Locales = new List<ILocaleProduct>
+                    {
+                        new LocaleProduct
+                        {
+                            IsoName = "dk",
+                            Gender = "Mand",
+                        },
+                    },
+                },
+                new Product
+                {
+                    Locales = new List<ILocaleProduct>
+                    {
+                        new LocaleProduct
+                        {
+                            IsoName = "dk",
+                            Gender = "Mand",
+                        },
+                    },
+                },
+            ]).Cast<IProduct>().ToList();
+        }
+    }
+}
